a2e292734cf70dbe2a532b8e6872f018a843b261
ebukaremmy <ebukaremmyikegwuonu@gmail.com>
2026-01-14 14:38:51 +0100

    Refactor prop drilling using Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,15 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    /* We provide the userData to any component inside this Provider */
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
