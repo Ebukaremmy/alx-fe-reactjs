e91bfb59113d3bc7e77ed18e17edd87a8d9e254d
ebukaremmy <ebukaremmyikegwuonu@gmail.com>
2026-01-18 02:03:10 +0100

    Task 2: Refactor prop drilling to Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,27 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import ProfilePage from './components/ProfilePage'
+import UserContext from './components/UserContext'
 
-function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
+const App = () => {
+
+  const userData = {name: 'Jane Doe', email: "jane.doe@example.com", age: 15, bio: 'Loves hiking and photography'};
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+    <div>
+      <Header />
+      <MainContent />
+      <WelcomeMessage />
+      <UserContext.Provider value={userData}>
+        <ProfilePage  />
+        <UserProfile />
+      </UserContext.Provider>
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
